819c802c3392524d1f25332be24ed3b452f34394
CPlant1 <n10465022@qut.edu.au>
2022-04-27 17:56:53 +1000

    Workshop 4 Updates

--- Clapper_ArduinoIDE_Base/Clapper_ArduinoIDE_Base.ino
+++ Clapper_ArduinoIDE_Base/Clapper_ArduinoIDE_Base.ino
@@ -1,20 +1,18 @@
-//This was my inspiration for the code:
-//https://www.youtube.com/user/greatscottlab
+///Clap- activated light switch code
 
-const int inputPin = 1; // The pin that the amplified sound signal will be read into the microcontroller
-const int buttonPin = 0; // The pin used to correct for different noise environments
-const int outputPin = 1; // The pin used to turn on the light/motor/relay/5V/9V 
+const int inputPin = 1; // The pin that the amplified sound signal will be read into the microcontroller (P2)
+const int buttonPin = 0; // The pin used to correct for different noise environments (P0)
+const int outputPin = 1; // The pin used to turn on the light/motor/relay/5V/9V (P1)
 
 //Setup Variables
 int sensorvalue=0;
 int eventstatus = 0;
-int sensorthresholdhigh = 750;
-int sensorthresholdlow = 730;
+int sensorthresholdhigh = 0;
 int threshold;
 
 // YOU MAY WANT TO CHANGE HERE
 // If clap is too sensitive, increase margin. Else, decrease margin
-int margin = 20;
+int margin = 300;
 
 
 //Setup Pins
@@ -22,7 +20,20 @@ void setup () {
   pinMode(outputPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   pinMode(inputPin+1, INPUT);
+
+
+//Set an initial value for the threshold
+  delay(2000);
+  int analogsum = 0;
+  for(int n=0;n<10;n++){
+    delay(10);
+    analogsum += analogRead(inputPin); 
+  }
+  threshold = analogsum/10; 
+  sensorthresholdhigh = threshold+margin;
+  delay(1000);
 }
+  
 
 void loop() { 
 
@@ -36,14 +47,12 @@ void loop() {
       }
     threshold = analogsum/10; 
     sensorthresholdhigh = threshold+margin;
-    sensorthresholdlow = threshold-margin;
     delay(1000);
   }
 
   // Read the amplified value of the microphone
   eventstatus = 0;  
   sensorvalue = analogRead(inputPin); 
-  delay(1);
 
   // Step 0: Detect a high signal
   if(eventstatus==0){ 
@@ -52,22 +61,20 @@ void loop() {
     }
   }
    // Step 1: Detect a low signal
-    if(eventstatus==1){  
-    for(int k =100; k>0;k--){
-      delay(1);
+  if(eventstatus==1){  
+    for(int k = 1000; k>0;k--){
       sensorvalue=analogRead(inputPin);
       if(sensorvalue<threshold){
         eventstatus=2; 
-        
         break;
       }      
     }
   }
   // Step 2: Detect a high signal from second clap
   if(eventstatus==2){
-    delay(500);
-    for(int n=1000;n>0;n--){
-      delay(1);
+    delay(100);
+    for(int n=5000;n>0;n--){
+      delayMicroseconds(100);
       sensorvalue=analogRead(inputPin);
       if(sensorvalue>sensorthresholdhigh){
         eventstatus=3;
